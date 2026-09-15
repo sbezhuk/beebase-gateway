bdd3a6e15e88fba1a0d38f726a55e0f984cf2e05
sbezhuk <sbezhuk@icloud.com>
2026-09-15 10:48:58 +0300

    feat: add reminders route to notification service

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -140,6 +140,7 @@ func NewRouter(log *slog.Logger, up Upstreams) http.Handler {
 	r.Mount("/api/v1/subscriptions", up.Subscription)
 	if up.Notification != nil {
 		r.Mount("/api/v1/devices", up.Notification)
+		r.Mount("/api/v1/reminders", up.Notification)
 	}
 
 	return r
